890274c60bad8085ee15d352e764ab8391f3cb3a
동우 <ddokkochi@gmail.com>
2024-08-14 11:15:18 +0900

    ADD : 창고관련 클래스 스텁추가

---
+++ src/services/WarehouseServiceImpl.java
@@ -0,0 +1,32 @@
+package services;
+
+import dao.warehouse.WarehouseDao;
+import dto.warehouse.Warehouse;
+import java.util.ArrayList;
+
+public class WarehouseServiceImpl {
+
+  private WarehouseDao warehouseDao;
+
+  public ArrayList<Warehouse> addWarehouseToList(Warehouse warehouse) {
+    Warehouse.getInstance().add(warehouse);
+    return Warehouse.getInstance();
+  }
+
+  public String readAllWh() {
+    return Warehouse.getInstance().toString();
+  }
+
+  public ArrayList<Warehouse> readByWhLocation() {
+    warehouseDao.selectWh();
+    return null;
+  }
+
+  public ArrayList<Warehouse> readByWhType() {
+    return null;
+  }
+
+  public ArrayList<Warehouse> readByWhName() {
+    return null;
+  }
+}
---
+++ src/vo/WhSection.java
@@ -0,0 +1,12 @@
+package vo;
+
+import lombok.Data;
+
+@Data
+public class WhSection {
+  private String sectionId;
+  private int capaWidth;
+  private int capaLength;
+  private int capaHeight;
+  private String reference;
+}
