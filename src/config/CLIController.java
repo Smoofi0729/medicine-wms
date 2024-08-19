4316f447250bee9bc094ad282f4c65e61904cfda
EdenKwon <ybinn@caresquare.kr>
2024-08-19 10:41:35 +0900

    FIX : 지출 변수명 수정 및 케이스 번호 수정

--- src/config/CLIController.java
+++ src/config/CLIController.java
@@ -103,8 +103,11 @@ public class CLIController {
                             releaseService.showReleasesForMall();
                     }
 
+<<<<<<< Updated upstream
 
                     validinput = true;
+=======
+>>>>>>> Stashed changes
                     break;
                 case 3:
                     System.out.println("재고 조회");
@@ -501,10 +504,10 @@ public class CLIController {
                 System.out.println("지출 카테고리:");
                 ex.setExpenditureCategory(SystemIn.SystemInString());
                 System.out.println("비고:");
-                newExpenditure.setNote(SystemIn.SystemInString());
-                es.insertExpenditure(newExpenditure);
+                ex.setNote(SystemIn.SystemInString());
+                es.insertExpenditure(ex);
                 break;
-            case 4:
+            case 5:
                 System.out.println("지출 수정");
                 Expenditure updateExpenditure = new Expenditure();
                 System.out.println("수정할 지출 ID:");
@@ -525,7 +528,7 @@ public class CLIController {
                 updateExpenditure.setNote(SystemIn.SystemInString());
                 es.updateExpenditure(updateExpenditure);
                 break;
-            case 5:
+            case 6:
                 System.out.println("지출 삭제");
                 Expenditure deleteExpenditure = new Expenditure();
                 System.out.println("삭제할 지출 ID:");
@@ -534,7 +537,7 @@ public class CLIController {
                 deleteExpenditure.setWarehouseId(SystemIn.SystemInString());
                 es.deleteExpenditure(deleteExpenditure);
                 break;
-            case 6:
+            case 7:
                 System.out.println("지출 조회 (ID)");
                 System.out.println("조회할 지출 ID:");
               
