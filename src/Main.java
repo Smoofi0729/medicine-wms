a37a19ab47ca76224ae1ec29c2a807ed141915f0
EdenKwon <ybinn@caresquare.kr>
2024-08-20 01:33:19 +0900

    FIX : 메인 메뉴 -> 로그인으로 실행하도록 변경

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import controller.CLIController;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //CLIController controller = new CLIController();
-        //controller.BasicMenu();
-
         CLIController controller = new CLIController();
-        controller.MemberMainMenu("ds");
+        controller.BasicMenu();
+
+        /*CLIController controller = new CLIController();
+        controller.MemberMainMenu("ds");*/
 
     }
 }
